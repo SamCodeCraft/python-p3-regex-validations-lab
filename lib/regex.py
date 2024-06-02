37976e56e70292a6fdb6fd397694c89f1d70785d
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-06-02 11:52:09 +0300

    RegEx Validations Lab

--- lib/regex.py
+++ lib/regex.py
@@ -3,11 +3,13 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
-name_regex = re.compile(name)
+import re
 
-phone_number = r""
-phone_regex = re.compile(phone_number)
+# Regex pattern for names
+name_regex = re.compile(r"^[A-Z][a-zA-Z]*([ '-][A-Z][a-zA-Z]*)*$")
 
-email_address = r""
-email_regex = re.compile(email_address)
+# Regex pattern for phone numbers
+phone_regex = re.compile(r"^(\d{10}|\(\d{3}\) \d{3}-\d{4}|\d{3}-\d{3}-\d{4})$")
+
+# Regex pattern for email addresses
+email_regex = re.compile(r"^[a-zA-Z][\w.]*@[a-zA-Z]+\.[a-zA-Z]+$")
